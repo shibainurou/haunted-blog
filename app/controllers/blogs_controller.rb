6b13beb5123536c0d723bcc95f6e3aee7d2cc80d
shibainurou <shibainurou@gmail.com>
2025-09-03 07:23:01 +0900

    リレーションを使ってuser_idではなくuserで条件を指定するように修正した

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -11,7 +11,7 @@ class BlogsController < ApplicationController
 
   def show
     # FROM BLOGS WHERE (SECREAT = FALSE OR USER_ID = ?) AND ID = ?
-    @blog = Blog.published.or(Blog.where(user_id: current_user&.id)).find(params[:id])
+    @blog = Blog.published.or(Blog.where(user: current_user)).find(params[:id])
   end
 
   def new
